e20e4eac7f8e3df3d9ec88cc9c0f51cb7ddd4298
Micheal Ataklt <matakltm.code@gmail.com>
2025-02-22 01:16:30 +0300

    wip: pint route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+declare(strict_types=1);
+
 use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -19,4 +20,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__ . '/auth.php';
+require __DIR__.'/auth.php';
